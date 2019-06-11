2305f00803cd7a1872bbf6b0c09cf75009953b27
psokolowicz <sokolowiczpaula@gmail.com>
2019-06-10 21:39:40 -0300

    .

--- familia.pl
+++ familia.pl
@@ -1,20 +1,3 @@
-persona(paula).
-persona(mirta).
-persona(perla).
-persona(daniela).
-persona(mor).
-persona(abi).
-persona(cecilia).
-persona(mercedes).
-persona(miguel).
-persona(pedro).
-persona(benjamin).
-persona(julian).
-persona(rene).
-persona(iam).
-persona(oren).
-persona(ruben).
-
 hijo(paula, mirta).
 hijo(paula, miguel).
 hijo(daniela, mirta).
@@ -46,43 +29,29 @@ pareja(mercedes, benjamin).
 
 hermanos(X, Y):-
     X \= Y,
-    persona(X),
-    persona(Y),
     hijo(X, Z),
     hijo(Y, Z).
 
 nietoAbuelo(X, Y):-
-    persona(X),
-    persona(Y),
     hijo(X, Z),
     hijo(Z, Y).
 
 sobrinoTio(X, Y):-
-    persona(X),
-    persona(Y),
     hijo(X, Z),
     hermanos(Y, Z).
 
 cuniados(X, Y):-
-    persona(X),
-    persona(Y),
     pareja(X, Z),
     hermanos(Z, Y).
 
 cuniados(X, Y):-
-    persona(X),
-    persona(Y),
     pareja(Z, X),
     hermanos(Z, Y).
 
 cuniados(X, Y):-
-    persona(X),
-    persona(Y),
     pareja(Z, Y),
     hermanos(X, Z).
 
 cuniados(X, Y):-
-    persona(X),
-    persona(Y),
     pareja(Y, Z),
     hermanos(X, Z).
